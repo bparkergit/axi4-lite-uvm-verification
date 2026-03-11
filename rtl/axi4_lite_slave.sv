fcf4cb500e52f469acb8ef0ba15a2c5c52e62608
Your Name <you@example.com>
2026-03-11 13:10:20 -0700

    rtl fix errors

--- rtl/axi4_lite_slave.sv
+++ rtl/axi4_lite_slave.sv
@@ -5,28 +5,15 @@ module axi4_lite_slave #(
     axi_if.DUT axi
 );
 
-//////////////////////////////////////////////
-// Register File
-//////////////////////////////////////////////
-
 logic [DATA_WIDTH-1:0] regfile [0:3];
-
-//////////////////////////////////////////////
-// Write Channel State
-//////////////////////////////////////////////
 
 logic [ADDR_WIDTH-1:0] awaddr_reg;
 logic [DATA_WIDTH-1:0] wdata_reg;
-
-logic awaddr_valid;
-logic wdata_valid;
-
-//////////////////////////////////////////////
-// Read Channel
-//////////////////////////////////////////////
-
 logic [ADDR_WIDTH-1:0] araddr_reg;
-logic araddr_valid;
+
+logic awaddr_valid, awaddr_valid_next;
+logic wdata_valid,  wdata_valid_next;
+logic araddr_valid, araddr_valid_next;
 
 //////////////////////////////////////////////
 // WRITE ADDRESS CHANNEL
@@ -38,18 +25,22 @@ always_ff @(posedge axi.aclk) begin
         awaddr_valid <= 0;
     end
     else begin
+        axi.s_axi_awready <= !awaddr_valid;
+        awaddr_valid <= awaddr_valid_next;
 
-        if(!awaddr_valid) begin
-            axi.s_axi_awready <= 1;
-
-            if(axi.s_axi_awvalid && axi.s_axi_awready) begin
-                awaddr_reg   <= axi.s_axi_awaddr;
-                awaddr_valid <= 1;
-                axi.s_axi_awready <= 0;
-            end
-        end
-
+        if(axi.s_axi_awvalid && axi.s_axi_awready)
+            awaddr_reg <= axi.s_axi_awaddr;
     end
+end
+
+always_comb begin
+    awaddr_valid_next = awaddr_valid;
+
+    if(axi.s_axi_awvalid && axi.s_axi_awready)
+        awaddr_valid_next = 1;
+
+    if(awaddr_valid && wdata_valid && axi.s_axi_bready && axi.s_axi_bvalid)
+        awaddr_valid_next = 0;
 end
 
 //////////////////////////////////////////////
@@ -62,22 +53,26 @@ always_ff @(posedge axi.aclk) begin
         wdata_valid <= 0;
     end
     else begin
+        axi.s_axi_wready <= !wdata_valid;
+        wdata_valid <= wdata_valid_next;
 
-        if(!wdata_valid) begin
-            axi.s_axi_wready <= 1;
-
-            if(axi.s_axi_wvalid && axi.s_axi_wready) begin
-                wdata_reg <= axi.s_axi_wdata;
-                wdata_valid <= 1;
-                axi.s_axi_wready <= 0;
-            end
-        end
-
+        if(axi.s_axi_wvalid && axi.s_axi_wready)
+            wdata_reg <= axi.s_axi_wdata;
     end
 end
 
+always_comb begin
+    wdata_valid_next = wdata_valid;
+
+    if(axi.s_axi_wvalid && axi.s_axi_wready)
+        wdata_valid_next = 1;
+
+    if(awaddr_valid && wdata_valid && axi.s_axi_bready && axi.s_axi_bvalid)
+        wdata_valid_next = 0;
+end
+
 //////////////////////////////////////////////
-// WRITE EXECUTION
+// WRITE RESPONSE
 //////////////////////////////////////////////
 
 always_ff @(posedge axi.aclk) begin
@@ -88,15 +83,9 @@ always_ff @(posedge axi.aclk) begin
     else begin
 
         if(awaddr_valid && wdata_valid && !axi.s_axi_bvalid) begin
-
             regfile[awaddr_reg[3:2]] <= wdata_reg;
-
             axi.s_axi_bvalid <= 1;
             axi.s_axi_bresp  <= 2'b00;
-
-            awaddr_valid <= 0;
-            wdata_valid  <= 0;
-
         end
 
         if(axi.s_axi_bvalid && axi.s_axi_bready)
@@ -115,18 +104,22 @@ always_ff @(posedge axi.aclk) begin
         araddr_valid <= 0;
     end
     else begin
+        axi.s_axi_arready <= !araddr_valid;
+        araddr_valid <= araddr_valid_next;
 
-        if(!araddr_valid) begin
-            axi.s_axi_arready <= 1;
-
-            if(axi.s_axi_arvalid && axi.s_axi_arready) begin
-                araddr_reg   <= axi.s_axi_araddr;
-                araddr_valid <= 1;
-                axi.s_axi_arready <= 0;
-            end
-        end
-
+        if(axi.s_axi_arvalid && axi.s_axi_arready)
+            araddr_reg <= axi.s_axi_araddr;
     end
+end
+
+always_comb begin
+    araddr_valid_next = araddr_valid;
+
+    if(axi.s_axi_arvalid && axi.s_axi_arready)
+        araddr_valid_next = 1;
+
+    if(araddr_valid && axi.s_axi_rready && axi.s_axi_rvalid)
+        araddr_valid_next = 0;
 end
 
 //////////////////////////////////////////////
@@ -142,13 +135,9 @@ always_ff @(posedge axi.aclk) begin
     else begin
 
         if(araddr_valid && !axi.s_axi_rvalid) begin
-
             axi.s_axi_rdata  <= regfile[araddr_reg[3:2]];
             axi.s_axi_rvalid <= 1;
             axi.s_axi_rresp  <= 2'b00;
-
-            araddr_valid <= 0;
-
         end
 
         if(axi.s_axi_rvalid && axi.s_axi_rready)
