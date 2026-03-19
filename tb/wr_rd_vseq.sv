49ef1d2da804172dc3eb6f3878d43596fb2c82ae
B Parker <pabr646@gmail.com>
2026-03-18 22:41:42 -0700

    Add wr_rd_vseq class for read/write sequences

---
+++ tb/wr_rd_vseq.sv
@@ -0,0 +1,38 @@
+class wr_rd_vseq extends uvm_sequence;
+  `uvm_object_utils(wr_rd_vseq)
+
+  `uvm_declare_p_sequencer(axi_virtual_sequencer)
+  
+  axi_read_sequence rd_seq;
+  axi_write_sequence wr_seq;
+   
+  bit [31:0] addr, data;
+  
+    
+  function new(string name = "wr_rd_vseq");   
+    super.new(name);  
+  endfunction
+  
+  
+  task body();
+        rd_seq = axi_read_sequence::type_id::create("rd_seq");
+  		wr_seq = axi_write_sequence::type_id::create("wr_seq");
+    
+    // ---------------- WRITE ----------------
+    wr_seq = axi_write_sequence::type_id::create("wr_seq");
+
+    wr_seq.start(p_sequencer.write_sequencer); 
+
+
+    addr  = wr_seq.addr;
+    data = wr_seq.data;
+
+    // ---------------- READ ----------------
+    rd_seq = axi_read_sequence::type_id::create("rd_seq");
+
+    rd_seq.addr = addr;
+
+    rd_seq.start(p_sequencer.read_sequencer); 
+    
+  endtask
+endclass
