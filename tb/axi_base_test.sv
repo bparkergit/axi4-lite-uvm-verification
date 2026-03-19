07a4061956ca4159072c09853574c6b70e4bd865
B Parker <pabr646@gmail.com>
2026-03-18 22:44:21 -0700

    Refactor axi_base_test class for sequence handling

--- tb/axi_base_test.sv
+++ tb/axi_base_test.sv
@@ -1,30 +1,37 @@
 class axi_base_test extends uvm_test;
   `uvm_component_utils(axi_base_test)
+  
 	axi_env env;
+    wr_rd_vseq vseq;
+  
     
-    function new(string name = "axi_base_test", uvm_component parent);
-          super.new(name,parent);
-        endfunction
+  function new(string name = "axi_base_test", uvm_component parent);     
+    super.new(name,parent);      
+  endfunction
       
-    
-    function void build_phase(uvm_phase phase);
+  
+  function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         env = axi_env::type_id::create("env", this);
-    endfunction
     
-        task run_phase(uvm_phase phase);
-        axi_base_sequence seq;
+  endfunction
+    
+      
+  task run_phase(uvm_phase phase);
 
         phase.raise_objection(this);
 
-          `uvm_info(get_type_name(), "Starting axi_base_sequence", UVM_LOW)
+          `uvm_info(get_type_name(), "Starting axi_write_sequence and axi_read_sequence", UVM_LOW)
 
-        seq = axi_base_sequence::type_id::create("seq");
-        seq.start(env.agent.sqr);
+          vseq = wr_rd_vseq::type_id::create("vseq");
+          
+          vseq.start(env.agent.vseqr);
+          
 
         #1000ns;   // give time to observe behavior
 
         phase.drop_objection(this);
-    endtask
+  
+  endtask
     
 endclass
