ea24f7befca874e2b67b2c328c3d3e072308d8cd
B Parker <pabr646@gmail.com>
2026-03-18 22:38:38 -0700

    Add axi_read_sequence class for AXI read operations
    Implement axi_read_sequence class for UVM sequence.

---
+++ tb/axi_read_sequence.sv
@@ -0,0 +1,34 @@
+class axi_read_sequence extends uvm_sequence #(axi_seq_item);
+  `uvm_object_utils(axi_read_sequence)
+  
+       
+  axi_seq_item item;   
+  bit [31:0] addr;
+  
+    function new(string name = "axi_base_sequence");
+        super.new(name);
+    endfunction
+  
+    task body();
+
+        
+      // READ
+      begin 
+            item = axi_seq_item::type_id::create("item");
+        
+            start_item(item);
+        
+            assert(item.randomize() with {
+                is_write == 0;
+                s_axi_araddr == addr;
+            }) else `uvm_error("RAND_FAIL", "Read randomization failed")
+
+              `uvm_info("SEQ", $sformatf("READ:  addr=0x%08h", item.s_axi_araddr), UVM_LOW)
+
+            finish_item(item);
+       end
+      
+      
+      
+    endtask
+endclass
