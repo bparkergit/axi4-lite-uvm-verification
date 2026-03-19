65cc621bfa9b96622a37da6b0c36f1caf6be6662
B Parker <pabr646@gmail.com>
2026-03-18 23:15:01 -0700

    Update axi_read_sequence.sv

--- tb/axi_read_sequence.sv
+++ tb/axi_read_sequence.sv
@@ -3,7 +3,7 @@ class axi_read_sequence extends uvm_sequence #(axi_seq_item);
   
        
   axi_seq_item item;   
-  bit [31:0] addr,data;
+  bit [31:0] addr;
   
     function new(string name = "axi_base_sequence");
         super.new(name);
@@ -26,7 +26,7 @@ class axi_read_sequence extends uvm_sequence #(axi_seq_item);
               `uvm_info("SEQ", $sformatf("READ:  addr=0x%08h", item.s_axi_araddr), UVM_LOW)
 
               addr = item.s_axi_araddr;       
-              data = item.s_axi_rdata;
+        
             finish_item(item);
         
 
