bb5085e415422eb1e88d8a7b61fcff7c52477bcf
B Parker <pabr646@gmail.com>
2026-03-18 22:49:38 -0700

    Refactor axi_agent to include multiple sequencers and drivers

--- tb/axi_agent.sv
+++ tb/axi_agent.sv
@@ -1,10 +1,13 @@
 class axi_agent extends uvm_agent;
   `uvm_component_utils(axi_agent)
   
-  axi_sequencer sqr;
-  axi_driver drv;
+  axi_sequencer wsqr;
+  axi_sequencer rsqr;
+  axi_wdriver wdrv;
+  axi_rdriver rdrv;
   axi_monitor mon;
   axi_coverage cov;
+  axi_virtual_sequencer vseqr;
   
   uvm_analysis_port #(axi_seq_item) ap;
  
@@ -17,9 +20,13 @@ class axi_agent extends uvm_agent;
   
       function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        sqr = axi_sequencer::type_id::create("sqr", this);
+
         if (is_active == UVM_ACTIVE) begin
-        	drv = axi_driver::type_id::create("drv", this);
+          wsqr = axi_sequencer::type_id::create("wsqr", this);
+          rsqr = axi_sequencer::type_id::create("rsqr", this);
+          wdrv = axi_wdriver::type_id::create("wdrv", this);
+          rdrv = axi_rdriver::type_id::create("rdrv", this);
+          vseqr = axi_virtual_sequencer::type_id::create("vseqr",this);
         end
         mon = axi_monitor::type_id::create("mon", this);
         ap = new("ap", this);
@@ -30,9 +37,13 @@ class axi_agent extends uvm_agent;
       function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
         if (is_active == UVM_ACTIVE) begin
-            drv.seq_item_port.connect(sqr.seq_item_export);
+          wdrv.seq_item_port.connect(wsqr.seq_item_export);
+          rdrv.seq_item_port.connect(rsqr.seq_item_export);
+          vseqr.write_sequencer = wsqr;
+          vseqr.read_sequencer = rsqr;
+   
         end
-      
+        
          mon.ap.connect(ap);
          mon.ap.connect(cov.analysis_export);
 
@@ -40,3 +51,4 @@ class axi_agent extends uvm_agent;
     endfunction
   
 endclass
+
